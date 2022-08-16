af6e81ea7e913fd1874641ae4fda9da4f890080d
LucasKripper <Kripperlm@gmail.com>
2022-08-16 17:14:11 -0300

    modificaciones en toda la App

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+const Header = () =>
+{
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Productos</a>
+                </li>
+                <li>
+                    <a href="#">Nosotros</a>
+                </li>
+                <li>
+                    <a href="#">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+}
+export default Header;
